6c391e960fc36b4a01393ae9b822e7e5aa2a0bc7
Alok Kumar <alokkumardasd2016@gmail.com>
2024-11-06 22:42:37 +0530

    cors

--- MongoUse/index.js
+++ MongoUse/index.js
@@ -8,13 +8,6 @@ const userRouter = require('./src/userDataGetFolder/user.allData');
 
 const app=express();
 app.use(cors());
-const cors = require('cors');
-app.use(cors({
-  origin: 'https://mongopratice.onrender.com',  // Replace with your frontend's actual domain
-  methods: ['GET', 'POST'],  // Allow GET and POST methods from the frontend
-  credentials: true,         // Allow cookies to be sent
-}));
-
 app.use(express.json());
 app.use("/user",userRouter);
 
